e48ef68f2fa3eedcabd44c7f8e1d1328aa414202
Allen Kao <allen.kao@roehl.life>
2020-08-17 14:42:43 +0800

    update dockerfile

--- util/util.py
+++ util/util.py
@@ -1,10 +1,7 @@
 import logging
 import os
 
-import pymysql
 import yaml
-from pymongo import MongoClient
-from sqlalchemy import create_engine
 
 from util.singleton import Singleton
 
@@ -19,28 +16,17 @@ class Util(metaclass=Singleton):
         with open("configs/config.yml") as config:
             config = yaml.load(config, Loader=yaml.FullLoader)
 
-            config["DB"]["host"] = (
-                os.environ.get("DB_HOST")
-                if os.environ.get("DB_HOST")
-                else config["DB"]["host"]
+            config["mongoDb"]["host"] = (
+                os.environ.get("DB_HOST") or config["mongoDb"]["host"]
             )
-
-            config["DB"]["port"] = (
-                int(os.environ.get("DB_PORT"))
-                if os.environ.get("DB_PORT")
-                else config["DB"]["port"]
+            config["mongoDb"]["port"] = (
+                os.environ.get("DB_PORT") or config["mongoDb"]["host"]
             )
-
-            config["DB"]["user"] = (
-                os.environ.get("DB_USERNAME")
-                if os.environ.get("DB_USERNAME")
-                else config["DB"]["user"]
+            config["mongoDb"]["user"] = (
+                os.environ.get("DB_USER") or config["mongoDb"]["user"]
             )
-
-            config["DB"]["password"] = (
-                os.environ.get("DB_PASSWORD")
-                if os.environ.get("DB_PASSWORD")
-                else config["DB"]["password"]
+            config["mongoDb"]["password"] = (
+                os.environ.get("DB_PASSWORD") or config["mongoDb"]["password"]
             )
 
         # overwrite configs by environment variable
@@ -54,38 +40,3 @@ class Util(metaclass=Singleton):
     @classmethod
     def get_config(cls):
         return Util().config
-
-    @classmethod
-    def get_db_connection(cls):
-        user = Util().config["DB"]["user"]
-        password = Util().config["DB"]["password"]
-        host = Util().config["DB"]["host"]
-        port = Util().config["DB"]["port"]
-        return pymysql.connect(
-            host=host,
-            user=user,
-            passwd=password,
-            port=port,
-            db=Util().config["DB"]["schema"],
-            charset="utf8",
-        )
-
-    @classmethod
-    def get_db_engine(cls):
-        user = Util().config["DB"]["user"]
-        password = Util().config["DB"]["password"]
-        host = Util().config["DB"]["host"]
-        port = Util().config["DB"]["port"]
-        return create_engine(
-            "mysql+pymysql://{}:{}@{}:{}".format(user, password, host, port)
-        )
-
-    @classmethod
-    def get_mongo_client(cls):
-        return MongoClient(
-            host=Util().config["mongoDb"]["host"],
-            port=Util().config["mongoDb"]["port"],
-            username=Util().config["mongoDb"]["username"],
-            password=Util().config["mongoDb"]["password"],
-            authMechanism="SCRAM-SHA-1",
-        )
